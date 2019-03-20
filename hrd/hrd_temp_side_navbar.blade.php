f4448ea24cced9d314d153df202717bc8fd310cd
raharj007 <rbuthorokunto@yahoo.com>
2019-03-20 10:04:20 +0700

    Add HRD

---
+++ hrd/hrd_temp_side_navbar.blade.php
@@ -0,0 +1,18 @@
+<div class="sidebar" id="sidebar">
+    <div class="sidebar-inner slimscroll">
+        <div id="sidebar-menu" class="sidebar-menu">
+            <ul>
+                <li class="menu-title">Main</li>
+                <li>
+                    <a href="../index-2.html"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a>
+                </li>
+                <li class="active">
+                    <a href="hrd_management_dokter.blade.php"><i class="fa fa-user-md"></i> <span>Dokter</span></a>
+                </li>
+                <li>
+                    <a href="../patients.html"><i class="fa fa-wheelchair"></i> <span>Patients</span></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
